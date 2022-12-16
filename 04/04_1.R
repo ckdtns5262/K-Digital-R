94739a23c92f84b4ea85a21dea97b0f26340a43c
ckdtns5262 <ckdtns5262@nate.com>
2022-12-16 18:03:17 +0900

    [12.16] R분석 업로드

--- 04/04_1.R
+++ 04/04_1.R
@@ -2,35 +2,118 @@
 library(dplyr)
 
 #데이터 불러오기(인구동향)
- 
+df = read.csv("C:/Rwork/04_인구동향.csv",  fileEncoding = "euc-kr")
 # 열명 확인
+names(df)
 # "행정구역별","시점","출생아수","사망자수","혼인건수","이혼건수"
- 
-#결측치 확인
- 
+names(df)<-c("행정구역별","시점","출생아수","사망자수","혼인건수","이혼건수") 
+
+head(df)
+# =>시점은 수치형으로 표현되어 있지만 범주형 자료로 보는게 효율적 , 범주형자료 : 행정구역별, 시점 / 수치형자료 : 나머지
+
+
+#EDA 
+#데이터 구조 확인
+str(df)
+df
+#범주형 변경 : 시점 int => category
+df$시점 <- as.factor(df$시점)
+mode(df$시점)
+class(df$시점)
+
+#결측치 확인 
+summary(df)
+is.na(df$출생아수)
+
 
 #결측치 내용 확인 
- 
+
+# is.na 사용
+df1 <- df[is.na(df$출생아수),]
+
+# filter 사용
+df2 <- df %>% filter(is.na(출생아수))
+
+unique(df2$행정구역별)
+unique(df2$시점)
+df2 
 #결측치 행 제거
- 
-#결측치 확인
- 
+df3 <- df %>%filter(!is.na(df$출생아수) & !is.na(df$사망자수) & 
+                      !is.na(df$혼인건수) & !is.na(df$이혼건수)) 
+
+df3 <-na.omit(df)
+summary(df3)
+summary(df)
 
 #결측치 값 대체
- 
+df4 <- df
+df4$출생아수 <- ifelse(is.na(df4$출생아수) , 0, df4$출생아수)
+summary(df4)
 
-#반복을 통한 na값 처리
- 
+#출생아수, 사망지수 , 혼인건수, 이혼건수에 대해
+# na 값 0으로 변경 : 반복문
+col <- names(df)[3:6]
+
+for(c in col) {
+  temp <-df[,c]
+  temp <- ifelse(is.na(temp),0, temp)
+  df[,c] <-temp
+}
+summary(df)
+
+df4<-df4 %>% replace(is.na(df4), 0)
+summary(df4)
 
 # 자연증가수 
- 
+df$자연증가수 <- df$출생아수-df$사망자수
+df$자연증가수
+
+df6<-df %>% filter(df$자연증가수 < 0)
+df6
+
+# case 1 
+df[df$행정구역별 =='전국' & df$자연증가수 <0,]
+
+# case 2 
+df[which(df$행정구역별 == '전국' & df$자연증가수 < 0),]
+
+# case 3 
+df %>% 
+  filter(df$행정구역별 =='전국' & df$자연증가수 <0)%>%
+  select('시점')
+
+
+
+
+
+
+# 데이터 분석
+# 기술통계분석- 범주형자료-빈도분석
+table(df$행정구역별)
+table(df$시점)
+# 기술통계분석- 범주형자료-빈도분석-barplot 그래프
+
+
+# 기술통계분석- 연속형자료-요약 통계량
+summary(df$출생아수)
+
+# 기술통계분석- 연속형자료-산점도 그래프
+plot(df$출생아수, df$혼인건수)
+
 # 전국 자료와 지역자료 분리
- 
-#데이터 분석
- 
+df_all<-df %>% filter(df$행정구역별 =="전국")
+df_loc<-df %>% filter(df$행정구역별 !="전국")
 
 # 전국데이터 자연 증가수 그래프
  
+
+  ggplot(mapping =aes(x=자연증가수 ,y=행정구역별, fill=시점 ), data=df_all) + 
+  geom_bar(stat="identity", position=position_dodge())  + 
+    coord_flip() +
+  ggtitle("전국데이터 자연 증가수 그래프")+
+  theme(plot.title = element_text(hjust = 0.5,size=20,face='bold'))
+
+
 
 library(ggplot2)
   
--- 04/04_2.R
+++ 04/04_2.R
@@ -1,16 +1,51 @@
 # 기상개황 자료를 이용하여 월별 불쾌지수를 계산하고 불쾌지수가 높음이상인 월을 구하시오.
 # 
 # https://kosis.kr/statHtml/statHtml.do?orgId=735&tblId=DT_A1040&vw_cd=MT_ZTITLE&list_id=215_215A_735_73503_A&seqNo=&lang_mode=ko&language=kor&obj_var_id=&itm_id=&conn_path=MT_ZTITLE
-# 
+df = read.csv("C:/Rwork/04_기상개황.csv",  fileEncoding = "euc-kr")
+#열명확인
+df<-df[,1:8]
+names(df)
+head(df)
+names(df)<-c("월별","평균기온","평균최고기온","최고기온","평균최저기온","최저극값기온",
+             "강수량","상대습도")
+names(df)
+df<-df[2:13,]
+df<-df[,2:8]
+# 마이너스이용해서 제거
+df<-df[-c(1:2)]
+df
 # 불쾌지수 공식
-# 
+df$불쾌지수<- 0.81 *df$평균기온 +0.01 * df$상대습도 *(0.99 *df$평균기온 - 14.3) + 46.3
+
+df$불쾌지수
+
 # DI = 0.81 * Ta + 0.01 * RH * (0.99 * Ta - 14.3) + 46.3
 # DI: 불쾌지수
 # Ta: 건구온도(평균기온)
 # RH: 상대습도(평균상대습도)
 # 불쾌지수 단계
-# 
+df$불쾌지수단계 <- ifelse(df$불쾌지수 >= 80, "매우높음",
+                    ifelse(df$불쾌지수 >=75, "높음",
+                         ifelse(df$불쾌지수 >=68, "보통","낮음"))) 
+df$불쾌지수단계
 # 매우높음: 80이상
 # 높음: 75이상 80미만
 # 보통: 68이상 75미만
 # 낮음: 68미만
+
+
+
+ggplot(mapping =aes(x=불쾌지수 ,y=월별, fill=불쾌지수단계 ), data=df) + 
+  geom_bar(stat="identity", position=position_dodge())  + 
+  coord_flip() +
+  ggtitle("월별 불쾌지수 단계")+
+  theme(plot.title = element_text(hjust = 0.5,size=20,face='bold'))
+
+
+
+install.packages('patchwork')
+
+
+
+
+
